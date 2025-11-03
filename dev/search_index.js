cbe1e20f1c2122fb5db9ce5289eec32a5cce3287
Documenter.jl <documenter@juliadocs.github.io>
2025-11-03 14:13:26 +0000

    build based on f127bc7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SBQT2025.jl","page":"SBQT2025.jl","title":"SBQT2025.jl","text":"","category":"section"},{"location":"","page":"SBQT2025.jl","title":"SBQT2025.jl","text":"Documentation for SBQT2025.jl","category":"page"}]
+}
